b5c40fd6c2114a3f8c3689ddbe1a83cbea86d87a
Andrew Lobban <andrewglobban@gmail.com>
2016-05-05 16:26:32 -0500

    Add spec for similarity

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,3 +1,4 @@
+require 'set'
 require 'card'
 
 describe Card do
@@ -16,6 +17,48 @@ describe Card do
 
   it 'has a rank' do
     raise unless card(rank: 4).rank == 4
+  end
+
+  it 'is equal to itself' do
+    subject = card(suit: :spades, rank: 4)
+    other   = card(suit: :spades, rank: 4)
+
+    raise unless subject == other
+  end
+
+  it 'is hash equal to itself' do
+    subject = card(suit: :spades, rank: 4)
+    other   = card(suit: :spades, rank: 4)
+
+    raise unless Set.new([subject, other]).size == 1
+  end
+
+  it 'is not equal to a card of differing suit' do
+    subject = card(suit: :spades, rank: 4)
+    other   = card(suit: :hearts, rank: 4)
+
+    raise unless subject != other
+  end
+
+  it 'is not hash equal to a card of differing suit' do
+    subject = card(suit: :spades, rank: 4)
+    other   = card(suit: :hearts, rank: 4)
+
+    raise unless Set.new([subject, other]).size == 2
+  end
+
+  it 'is not equal to a card of differing rank' do
+    subject = card(suit: :spades, rank: 4)
+    other   = card(suit: :spades, rank: 5)
+
+    raise unless subject != other
+  end
+
+  it 'is not hash equal to a card of differing rank' do
+    subject = card(suit: :spades, rank: 4)
+    other   = card(suit: :spades, rank: 5)
+
+    raise unless Set.new([subject, other]).size == 2
   end
 
   describe 'a jack' do
