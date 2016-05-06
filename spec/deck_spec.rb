06879c64971bcad968655ca9e12cf8c582f93511
Andrew Lobban <andrewglobban@gmail.com>
2016-05-06 08:16:21 -0500

    Add deck spec

---
+++ spec/deck_spec.rb
@@ -0,0 +1,16 @@
+require 'rspec'
+
+describe 'Deck' do
+  describe '.all' do
+    it 'contains 32 cards' do
+      expect(Deck.all.length).to eq(32)
+    end
+
+    it 'has a seven as its lowest card' do
+      Deck::SUITS.each do |suit|
+        expect(Deck.all).to include(Card.build(suit, 7))
+        expect(Deck.all).to_not include(Card.build(suit, 6))
+      end
+    end
+  end
+end
